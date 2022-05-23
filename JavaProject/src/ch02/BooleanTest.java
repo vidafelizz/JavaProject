6b968c9903f4b9336c135044c9c8796cc7f07afc
mia <mia@DESKTOP-KQFAL51>
2022-05-23 20:57:16 +0900

    Boolean타입

---
+++ JavaProject/src/ch02/BooleanTest.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class BooleanTest {
+	public static void main(String[] args) {
+		boolean b; // 1byte짜리 데이터타입 true/false
+		b = true;
+		System.out.println("b: " + b);
+		
+		b = (1 > 2);
+		System.out.println("b: " + b);
+	}
+}
