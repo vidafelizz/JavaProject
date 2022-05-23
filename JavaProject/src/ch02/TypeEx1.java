3ecce42911c685953fa2fc6c70793f92dea10308
mia <mia@DESKTOP-KQFAL51>
2022-05-23 20:55:05 +0900

    소리가 1시간 동안 가는 거리 출력

---
+++ JavaProject/src/ch02/TypeEx1.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class TypeEx1 {
+	public static void main(String[] args) {
+		int mach;
+		int distance;
+		
+		mach = 340;
+		distance = mach*60*60;
+		System.out.println("소리가 1시간 동안 가는 거리 : " + distance + "m");
+	}
+}
