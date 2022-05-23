968e0eaed5158a3c3b272c9f7162b1c808e8b2dd
mia <mia@DESKTOP-KQFAL51>
2022-05-23 20:55:32 +0900

    원의 넓이 구하기

---
+++ JavaProject/src/ch02/TypeEx2.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class TypeEx2 {
+	public static void main(String[] args) {
+		double radius;
+		double area;
+		
+		radius = 10.0;
+		area = radius*radius*3.14;
+		System.out.println("반지름이 " + radius + "인 원의 넓이 : " + area);
+	}
+}
