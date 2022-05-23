c39341a33282b9b2b84f6381571a470cafb3cce5
mia <mia@DESKTOP-KQFAL51>
2022-05-23 20:41:13 +0900

    데이터타입 long을 이용한 빛이 1년 동안 가는 거리 계산하기

---
+++ JavaProject/src/ch02/Light.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Light {
+	public static void main(String[] args) {
+		long lightspeed; // long은 64비트(8바이트) 정수를 저장
+		long distance;
+		
+		lightspeed = 30000L;
+		distance = lightspeed*365*24*60*60;
+		
+		System.out.println("빛이 1년 동안 가는 거리 : " + distance);
+	}
+}
