25f4efb4a15a96e7c128affddb4b15ef96901115
mia <mia@DESKTOP-KQFAL51>
2022-05-23 22:16:16 +0900

    자료형변환을 이용한 산술 연산

---
+++ JavaProject/src/ch03/CastEx3.java
@@ -0,0 +1,13 @@
+package ch03;
+
+// println()문과는 달리 printf에서는 결과값을 %.1f로 실수형식으로 지정해 놓은 경우는
+// 반드시 연산 결과가 실수 값으로 나와야 하기 때문에 형변환 연산자인(double)을
+// 명시적으로 적어 실수형으로 맞추어 주어야 한다.
+public class CastEx3 {
+	public static void main(String[] args) {
+		int x = 8;
+		int y = 5;
+		System.out.println("x/y=" + x/y);
+		System.out.printf("x/y=%.1f", (double)x/y);	
+	}
+}
