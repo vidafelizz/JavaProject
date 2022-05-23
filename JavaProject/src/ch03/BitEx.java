8a85a02896bd22cc537388db6444a035a833f40d
mia <mia@DESKTOP-KQFAL51>
2022-05-23 22:18:07 +0900

    비트 연산자

---
+++ JavaProject/src/ch03/BitEx.java
@@ -0,0 +1,10 @@
+package ch03;
+
+// 비트연산자( | or, &and, ^Xor )
+public class BitEx {
+	public static void main(String[] args) {
+		System.out.println(3 | 5); // or연산
+		System.out.println(3 & 5); // and연산
+		System.out.println(3 ^ 5); // Xor연산
+	}
+}
