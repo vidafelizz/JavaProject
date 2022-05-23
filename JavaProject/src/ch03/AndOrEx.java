89c1d1b2573e6c78dfa33ec7f35ae85f06f3f36a
mia <mia@DESKTOP-KQFAL51>
2022-05-23 21:54:57 +0900

    논리 연산자

---
+++ JavaProject/src/ch03/AndOrEx.java
@@ -0,0 +1,9 @@
+package ch03;
+
+public class AndOrEx {
+	public static void main(String[] args) {
+		int a = 7;
+		System.out.println(a>5 && a<0);		// true && false = false
+		System.out.println(a > 5 || a < 0); // true || false = true
+	}
+}
