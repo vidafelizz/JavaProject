94a2c4f4e90cf08b9802d6a7be0b198281376bbb
mia <mia@DESKTOP-KQFAL51>
2022-05-23 21:53:26 +0900

    비교연산자와 대입연산자

---
+++ JavaProject/src/ch03/CompareEx.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class CompareEx {
+	public static void main(String[] args) {
+		int a = 65;
+		char c = 'A';
+		System.out.println(a == c); // 비교연산자(Equal)
+		System.out.println(a = c);  // 대입연산자
+		System.out.println('A' != 65);
+	}
+}
