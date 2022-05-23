ca1e0c7471e978833d83fce3c28ef5126a1efdd1
mia <mia@DESKTOP-KQFAL51>
2022-05-23 22:00:08 +0900

    String 합 구하기

---
+++ JavaProject/src/ch03/StringExam.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class StringExam {
+	public static void main(String[] args) {
+		String str = "Hello" + " java"; // 문자열을 연결하는 연결연산자 "+"
+		System.out.println(str);
+		
+		str = 123 + "C"; // 숫자 + 문자열
+		System.out.println(str);
+	}
+}
