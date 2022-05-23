d5b743db9dbe7e0d059068fc79225c73ca15478c
mia <mia@DESKTOP-KQFAL51>
2022-05-23 20:33:19 +0900

    지역변수 초기화

---
+++ JavaProject/src/ch02/VarInitTest.java
@@ -0,0 +1,10 @@
+package ch02;
+
+// 지역변수는 반드시 초기화(값을 할당)해야한다.
+public class VarInitTest {
+	public static void main(String[] args) {
+		int index = 20;
+		index = index + 1;
+		System.out.println("index : " + index);
+	}
+}
