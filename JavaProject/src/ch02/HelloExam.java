d11523be059a0ce64ca31ae0421f743d25c79cb6
mia <mia@DESKTOP-KQFAL51>
2022-05-23 20:42:02 +0900

    메서드 이용해서 정수 합 출력하기

---
+++ JavaProject/src/ch02/HelloExam.java
@@ -0,0 +1,22 @@
+package ch02;
+
+//클래스의 이름은 반드시 대문자로 시작
+//메서드의 이름은 반드시 소문자로 시작
+//변수이름도 소문자로 시작
+public class HelloExam {
+	public static int sum(int n, int m) {
+		return n+m; // return은 결과값을 호출한 쪽으로 반환한다.
+	}
+	public static void main(String[] args) {
+		int i=20;
+		int s;
+		char a;
+		
+		s = sum(i,10);
+		a = '?';
+		
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+	}
+}
