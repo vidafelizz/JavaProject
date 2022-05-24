eb95e0ff63c0ff90d754fe9ef697ecadacd3485c
mia <mia@DESKTOP-KQFAL51>
2022-05-24 16:50:40 +0900

    if문 이용해서 트리모양으로 알파벳 출력

---
+++ JavaProject/src/ch04/Char_Tower.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class Char_Tower {
+	public static void main(String[] args) {
+		char a = 65; // 65는 아스키코드로'A'
+		for(int i=1; i<5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(a++);
+			}
+			System.out.println();
+		}
+	}
+}
