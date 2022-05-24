dc637828877f2c56d3069fdb05719035822c245d
mia <mia@DESKTOP-KQFAL51>
2022-05-24 16:12:03 +0900

    if문 이용해서 1부터 100까지(10,20,30..100단위로) 자연수 합계 출력

---
+++ JavaProject/src/ch04/For_Sum.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class For_Sum {
+	public static void main(String[] args) {
+		int total = 0;
+		for(int i=1; i<=100; i++) {
+			total += i;
+			if(i%10 == 0) {
+				System.out.println("1부터 " + i + "까지의 자연수 합계 : " + total);
+			}
+		}
+	}
+}
