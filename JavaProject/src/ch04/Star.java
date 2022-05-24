c352e9c9c22165b631ff85cc7e86fb42d0c827a2
mia <mia@DESKTOP-KQFAL51>
2022-05-24 16:40:23 +0900

    for문 이용해서 별찍기

---
+++ JavaProject/src/ch04/Star.java
@@ -0,0 +1,19 @@
+package ch04;
+
+public class Star {
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		// 역삼각형
+		for(int i=4; i>=1; i--) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
