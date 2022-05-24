d6bdd3a63589d83d42d55ec09d685ddd905e4eb9
mia <mia@DESKTOP-KQFAL51>
2022-05-24 16:52:22 +0900

    while문

---
+++ JavaProject/src/ch04/WhileEx.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class WhileEx {
+	public static void main(String[] args) {
+		int i = 1;
+		while(i<=10) {
+			System.out.println(i);
+			i++;
+		}
+		for(int j=1; j<=10; j++) {
+			System.out.println(j);
+		}
+	}
+}
