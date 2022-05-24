856090d86c0dede4a79ef868dfad87e86da5e5b0
mia <mia@DESKTOP-KQFAL51>
2022-05-24 15:31:57 +0900

    if문

--- JavaProject/src/ch04/If_Exam.java
+++ JavaProject/src/ch04/If_Exam.java
@@ -4,8 +4,8 @@ public class If_Exam {
 	public static void main(String[] args) {
 		int a = 10;
 		if(a%2 == 0) {
-			System.out.println(a + "짝수입니다.");
+			System.out.println(a + " ==> 짝수입니다.");
 		}
-		System.out.println("if문이 끝난이후 그 다음 명령문을 수행합니다.");
+		System.out.println("if문이 끝난 이후 그 다음 명령문을 수행합니다.");
 	}
 }
