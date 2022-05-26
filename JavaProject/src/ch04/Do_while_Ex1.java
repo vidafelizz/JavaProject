98e12ba0ca680ebd2dd29e8f760c7aa13fadad69
vidafelizz <yoonooy99@gmail.com>
2022-05-26 15:55:44 +0900

    do~while문

---
+++ JavaProject/src/ch04/Do_while_Ex1.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class Do_while_Ex1 {
+	public static void main(String[] args) {
+		int i = 1;
+		do { // 먼저 실행
+			System.out.print(i);
+			i++;
+		} while(i<=10); // 나중에 검사
+	}
+}
