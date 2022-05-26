14beace7a88fc5b3449586d2063e2c881423da07
vidafelizz <yoonooy99@gmail.com>
2022-05-26 16:20:23 +0900

    break문(2)

---
+++ JavaProject/src/ch04/BreakEx2.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class BreakEx2 {
+	public static void main(String[] args) {
+		int i = 1;
+		while(i<10) { // 1~9까지 반복
+			if(i==5) break;
+			System.out.println("i=" + i);
+			i++;
+		}
+		System.out.println("**반복문 종료**");
+	}
+}
