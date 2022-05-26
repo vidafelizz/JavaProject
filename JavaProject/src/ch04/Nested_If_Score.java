f8d6765d33fdb27ed7e295cdc2e7da63bc38d18d
vidafelizz <yoonooy99@gmail.com>
2022-05-26 18:03:16 +0900

    중첩 for문 이용해서 성적과 등급 출력

---
+++ JavaProject/src/ch04/Nested_If_Score.java
@@ -0,0 +1,36 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class Nested_If_Score {
+	public static void main(String[] args) {
+		int score = 0;
+		char grade = ' ';
+		char level = '0'; // A0, B0 문자로 처리
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Java 점수 : ");
+		score = sc.nextInt();
+		sc.close();
+		
+		if(score >= 90) {
+			grade = 'A';
+			if(score > 97) {
+				level = '+';
+			} else if(score < 94) {
+				level = '-';
+			}
+		} else if(score >= 80) {
+			grade = 'B';
+			if(score > 87) {
+				level = '+';
+			} else if(score < 84) {
+				level = '-';
+			}
+		} else {
+			grade = 'F';
+			System.out.println("재수강 대상입니다.");
+		}
+		System.out.println("당신의 점수는 " + score + "이고, 등급은 "+ grade + level + "입니다.");
+	}
+}
