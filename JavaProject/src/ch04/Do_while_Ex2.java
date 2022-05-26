ffc537f5cb4348e5f41e3d04a4054d0dac3c772f
vidafelizz <yoonooy99@gmail.com>
2022-05-26 15:58:54 +0900

    do-while문(2)

---
+++ JavaProject/src/ch04/Do_while_Ex2.java
@@ -0,0 +1,12 @@
+package ch04;
+
+public class Do_while_Ex2 {
+	public static void main(String[] args) {
+		int n = 1;
+		do {
+			System.out.println(n + " Hello World");
+			n++;
+		} while(n<=10);
+		System.out.println("while문 빠져나옴");
+	}
+}
