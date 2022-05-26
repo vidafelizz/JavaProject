357c1dd892daca05bfddcdecbb7b3e312b6a84b9
vidafelizz <yoonooy99@gmail.com>
2022-05-26 16:48:40 +0900

    while문 이용해서 메뉴 선택하기

--- JavaProject/src/ch04/WhileMenu.java
+++ JavaProject/src/ch04/WhileMenu.java
@@ -9,9 +9,7 @@ public class WhileMenu {
 		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
-			System.out.println("(1) 아메리카노");
-			System.out.println("(2) 카페라떼");
-			System.out.println("(3) 에스프레소");
+			System.out.println("(1)아메리카노 (2)카페라떼 (3)에스프레소");
 			System.out.println("원하는 메뉴(1~3)을 선택하세요.(종료:0)");
 			String tmp = sc.next(); // 문자처리
 			menu = Integer.parseInt(tmp); // 입력받은 문자를 숫자로 변환
@@ -24,6 +22,7 @@ public class WhileMenu {
 				continue;
 			}
 			System.out.println("선택하신 메뉴는 " + menu + "번 입니다.");
+			System.out.println();
 		}
 		sc.close();
 	}
