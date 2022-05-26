a7bf8ca1b1191075c5d37fddbbc7c3cd24660e53
vidafelizz <yoonooy99@gmail.com>
2022-05-26 16:40:30 +0900

    continue문 이용해서 홀수만 출력

--- JavaProject/src/ch04/ContinueEx2.java
+++ JavaProject/src/ch04/ContinueEx2.java
@@ -4,7 +4,7 @@ public class ContinueEx2 {
 	public static void main(String[] args) {
 		for(int i=0; i<10; i++) {
 			if(i%2==0) continue;
-			System.out.println(i);		
+			System.out.println("홀수 값: " + i);		
 		}
 	}
 }
