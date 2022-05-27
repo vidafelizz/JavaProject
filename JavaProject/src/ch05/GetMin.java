5317e965664dcee14a805ebb0883bd1e6344a150
vidafelizz <yoonooy99@gmail.com>
2022-05-27 16:38:39 +0900

    배열 안에서 최소값 구하기

---
+++ JavaProject/src/ch05/GetMin.java
@@ -0,0 +1,15 @@
+package ch05;
+
+public class GetMin {
+	public static void main(String[] args) {
+		int s[] = {12,3,19,6,18,8,12,4,1,19};
+		int minimum;
+		minimum = s[0];
+		for(int i=1; i<s.length; i++) {
+			if(s[i] < minimum) {
+				minimum = s[i];
+			}
+		}
+		System.out.println("최소값은 " + minimum);
+	}
+}
