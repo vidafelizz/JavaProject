b062e87951169e8e9fa294ed6057b80aef2b225b
vidafelizz <yoonooy99@gmail.com>
2022-05-30 20:51:04 +0900

    .length메서드 이용해서 입력 받은 배열의 평균 구하기

---
+++ JavaProject/src/ch05/ArrayLength.java
@@ -0,0 +1,19 @@
+package ch05;
+
+import java.util.Scanner;
+
+public class ArrayLength {
+	public static void main(String[] args) {
+		int intArray[] = new int[5];
+		int sum=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println(intArray.length + "개의 정수를 입력하세요 >>");
+		for(int i=0; i<intArray.length; i++) {
+			intArray[i] = sc.nextInt();
+		}
+		for(int i=0; i<intArray.length; i++) {
+			sum += intArray[i]; // 배열에 저장된 정수
+		}
+		System.out.println("평균은 " + (double)sum/intArray.length);
+	}
+}
