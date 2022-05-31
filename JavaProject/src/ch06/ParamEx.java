cd3d880eec72751961a65d5964775e10bba384c7
vidafelizz <yoonooy99@gmail.com>
2022-05-31 21:07:22 +0900

    산술연산자 메서드

---
+++ JavaProject/src/ch06/ParamEx.java
@@ -0,0 +1,27 @@
+package ch06;
+
+public class ParamEx {
+	static int plus(int a, int b) {
+		return a+b;
+	}
+	static int minus(int a, int b) {
+		return a-b;
+	}
+	static int multi(int a, int b) {
+		return a*b;
+	}
+	static int divide(int a, int b) {
+		return a/b;
+	}
+	public static void main(String[] args) {
+		int n;
+		n = plus(10,20);
+		System.out.println("plus : " + n);
+		n = minus(100, 50);
+		System.out.println("minus : " + n);
+		n = multi(10, 5);
+		System.out.println("multi : " + n);
+		n = divide(10, 5);
+		System.out.println("divide : " + n);
+	}
+}
