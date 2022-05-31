cb5e37461e8c9717e06176dc34ff8ff33d01689f
vidafelizz <yoonooy99@gmail.com>
2022-05-31 21:04:00 +0900

    abs 메서드 호출

---
+++ JavaProject/src/ch06/AbsEx.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class AbsEx {
+	public static int abs(int num) {
+		return num > 0 ? num : -num;
+	}
+	public static void main(String[] args) {
+		int a = -15;
+		System.out.println(a);
+		System.out.println(abs(a));
+		System.out.println(Math.abs(a));
+	}
+}
