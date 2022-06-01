38b3f0ef8f180206422077ceb2fc6100937767f3
vidafelizz <yoonooy99@gmail.com>
2022-06-01 13:57:03 +0900

    원의 반지름과 넓이 구하는 메서드 생성하고, 객체 생성 후 호출하기

---
+++ JavaProject/src/ch07/CircleDemo.java
@@ -0,0 +1,19 @@
+package ch07;
+
+public class CircleDemo {
+	public static void main(String[] args) {
+		Circle myCircle = new Circle();
+		myCircle.radius = 10;
+		myCircle.show(myCircle.radius, myCircle.findArea());
+	}
+}
+class Circle {
+	double radius;
+	
+	double findArea() {
+		return 3.14 * radius * radius;
+	}
+	void show(double x, double y) {
+		System.out.printf("반지름 = %.1f, 넓이 = %.1f\n", x, y);
+	}
+}
