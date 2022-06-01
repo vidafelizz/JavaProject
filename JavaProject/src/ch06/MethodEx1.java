ba6ef61bffb570a01e2c8439518781a5f74e5916
vidafelizz <yoonooy99@gmail.com>
2022-06-01 13:00:08 +0900

    메서드를 사용하지 않을 경우

---
+++ JavaProject/src/ch06/MethodEx1.java
@@ -0,0 +1,24 @@
+package ch06;
+
+public class MethodEx1 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i=0; i<=10; i++) {
+			sum += i;
+		}
+		System.out.println("합(1~10) : " + sum);
+		
+		sum = 0;
+		for(int i=10; i<=100; i++) {
+			sum += i;
+		}
+		System.out.println("합(10~100) : " + sum);
+		
+		sum = 0;
+		for(int i=100; i<=1000; i++) {
+			sum += i;
+		}
+		System.out.println("합(100~1000) : " + sum);
+	}
+
+}
