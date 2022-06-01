2f6d0615810a961ef8d59315ce816be35081acb5
vidafelizz <yoonooy99@gmail.com>
2022-06-01 15:06:35 +0900

    객체 생성 후 메서드 호출하기

---
+++ JavaProject/src/ch07/StudentUse2.java
@@ -0,0 +1,9 @@
+package ch07;
+
+public class StudentUse2 {
+	public static void main(String[] args) {
+		Student2 s2 = new Student2();
+		s2.input("김민수", "컴퓨터", 1, "123456", 4.3, 500);
+		s2.print();
+	}
+}
