0e68f1be0443b5ee5d4846b712070a2443281560
vidafelizz <yoonooy99@gmail.com>
2022-06-01 13:38:09 +0900

    현재 채널 메서드로 불러오기

---
+++ JavaProject/src/ch07/TelevisionTest4.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class TelevisionTest4 {
+	public static void main(String[] args) {
+		Television myTv = new Television();
+		myTv.channel = 24;
+		myTv.volume = 30;
+		myTv.onOff = true;
+		int ch = myTv.getChannel();
+		
+		System.out.println("현재 채널은 " + ch + "입니다.");
+	}
+
+}
