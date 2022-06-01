c804639f49dd56e9109d6b1c9938365466aa2634
vidafelizz <yoonooy99@gmail.com>
2022-06-01 14:51:42 +0900

    객체 생성 후 메서드 호출

---
+++ JavaProject/src/ch07/PointUse.java
@@ -0,0 +1,12 @@
+package ch07;
+
+public class PointUse {
+	public static void main(String[] args) {
+		Point p1 = new Point();
+		p1.setName("홍길동");
+		p1.setKor(90);
+		p1.setEng(80);
+		p1.setMat(89);
+		p1.print();
+	}
+}
