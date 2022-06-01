2f7561303a709250db5432a11283d22bff6ac45f
vidafelizz <100111300+vidafelizz@users.noreply.github.com>
2022-06-01 14:15:42 +0900

    getter&setter 이용해서 메서드 생성하기

--- JavaProject/src/ch07/TelevisionTest5.java
+++ JavaProject/src/ch07/TelevisionTest5.java
@@ -6,5 +6,5 @@ public class TelevisionTest5 {
 		myTv.setChannel(11); // setter를 통해 값을 초기화(저장)
 		int ch = myTv.getChannel(); // getter를 통해 값을 불러옴 ch = 11
 		System.out.println("현재 채널은 " + ch + "입니다.");
-	}
+	} 
 }
