be55955aea4842de000fa5139c0129db40b9a9db
vidafelizz <yoonooy99@gmail.com>
2022-06-01 13:39:34 +0900

    getter&setter 이용해서 메서드 호출하기

---
+++ JavaProject/src/ch07/TelevisionTest5.java
@@ -0,0 +1,10 @@
+package ch07;
+
+public class TelevisionTest5 {
+	public static void main(String[] args) {
+		Television myTv = new Television(); // 객체생성
+		myTv.setChannel(11); // setter를 통해 값을 초기화(저장)
+		int ch = myTv.getChannel(); // getter를 통해 값을 불러옴 ch = 11
+		System.out.println("현재 채널은 " + ch + "입니다.");
+	}
+}
