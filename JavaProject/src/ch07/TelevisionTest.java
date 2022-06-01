3bd9d08a40d20fbe891f7ae1fd32d8fdb433a969
vidafelizz <yoonooy99@gmail.com>
2022-06-01 13:32:03 +0900

    tv 객체 생성하고 메서드 호출

---
+++ JavaProject/src/ch07/TelevisionTest.java
@@ -0,0 +1,11 @@
+package ch07;
+
+public class TelevisionTest {
+	public static void main(String[] args) {
+		Television tv = new Television();
+		tv.channel = 7;
+		tv.volume = 9;
+		tv.onOff = true;
+		System.out.println("TV의 채널은 " + tv.channel + "이고, 볼륨은 " + tv.volume + "입니다.");
+	}
+}
