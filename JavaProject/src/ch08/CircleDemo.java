9f8ca0f098edffee798e8d6ea3746783e3ed739c
vidafelizz <yoonooy99@gmail.com>
2022-06-02 19:10:59 +0900

    원의 반지름과 색깔을 출력하는 생성자 생성

---
+++ JavaProject/src/ch08/CircleDemo.java
@@ -0,0 +1,40 @@
+package ch08;
+
+class Circle {
+	private double radius;
+	private String color;
+	
+	public Circle(double r, String C) {
+		radius = r;
+		color = "c";
+	}
+	public Circle(double r) {
+		radius = r;
+		color = "파랑";
+	}
+	public Circle(String c) {
+		radius = 10.0;
+		color = c;
+	}
+	public Circle() {
+		radius = 10.0;
+		color = "빨강";
+	}
+	public void printf() {
+		System.out.println("반지름 : " + radius);
+		System.out.println("색 깔 : " + color);
+		System.out.println();
+	}
+}
+public class CircleDemo {
+	public static void main(String[] args) {
+		Circle c1 = new Circle(10.0, "빨강");
+		c1.printf();
+		
+		Circle c2 = new Circle(5.0);
+		c2.printf();
+		
+		Circle c3 = new Circle("노랑");
+		c3.printf();
+	}
+}
